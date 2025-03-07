428e447ac5602fcbbdc59a154d6bfddcf44e8d09
jackxuan0305 <99300718+jackxuan0305@users.noreply.github.com>
2025-03-07 13:44:10 +0800

    Update FlutterStethoPlugin.java

--- android/src/main/java/com/brianegan/flutterstetho/FlutterStethoPlugin.java
+++ android/src/main/java/com/brianegan/flutterstetho/FlutterStethoPlugin.java
@@ -27,7 +27,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+// import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class FlutterStethoPlugin implements FlutterPlugin, MethodCallHandler {
     public final static String TAG = "FlutterStethoPlugin";
